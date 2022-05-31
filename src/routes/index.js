4320701864a3b42d06629b2b8ad98bc8a8bd06b6
braian-brb <braian.bilicic@gmail.com>
2022-05-31 12:48:42 -0300

    add test-faker /api/products-test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,21 @@
 import { Router } from 'express';
+import { faker } from '@faker-js/faker';
 
 export const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('home');
-  });
+  res.render('home');
+});
+
+// ****************************** TEST FAKER ****************************
+router.get('/api/products-test', async (req, res) => {
+  const productsFaker = [];
+  for (let i = 0; i < 5; i++) {
+    productsFaker.push({
+      name: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: faker.image.image(),
+    });
+  }
+  res.render('table', { products: productsFaker });
+});
