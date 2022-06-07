1d1641b0dc4ef578e2cea167cbd41e3180fd3d96
Braian Bilicic <braian.bilicic@gmail.com>
2022-06-06 23:45:43 -0300

    setting port with flag --port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { app, httpServer } from './app.js'
+import { httpServer } from './app.js'
+import { PORT } from './params.js'
 import './database.js'
 
-const PORT = app.get('PORT')
+// const PORT = app.get('PORT')
 
 httpServer.listen(PORT, () => {
   console.log(`Server on http://localhost:${PORT}`)
