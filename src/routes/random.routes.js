dc28128108602ccde3fd647a202b60ae51aeb682
Braian Bilicic <braian.bilicic@gmail.com>
2022-06-20 17:35:00 -0300

    update random number

--- src/routes/random.routes.js
+++ src/routes/random.routes.js
@@ -1,12 +1,17 @@
 import { Router } from 'express'
 import { fork } from 'child_process'
+import config from '../config/index.config.js'
+
 export const router = Router()
 
 router.get('/', (req, res) => {
-  const { cant = 100000000 } = req.query
+  const PORT = config.PORT
+  // mayuscula
+  const { CANT = 100000000 } = req.query
+  console.log(CANT)
   const randomChild = fork('./src/child_process/random.child.js')
-  randomChild.send(cant)
+  randomChild.send(CANT)
   randomChild.on('message', result => {
-    res.json({ result })
+    res.render('apiRandom/apiRandom', { result, PORT })
   })
 })
